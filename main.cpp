e88c16d2261b384aecd9a9b31b63ed48586235d7
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-07-09 17:26:40 -0400

    Update main.cpp
    One line solution.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int startIndex = 0;
-        int endIndex = s.size() - 1;
-        
-        while (startIndex <= endIndex) {
-            char temp = s[startIndex];
-            
-            s[startIndex] = s[endIndex];
-            s[endIndex] = temp;
-            startIndex++;
-            endIndex--;
-        }
+        reverse(s.begin(), s.end());
     }
 };
